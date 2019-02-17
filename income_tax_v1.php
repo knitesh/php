90c84914eb521ccbf5f71d01fe55bc8677a7cec2
Kumar Nitesh <contact@kumar-nitesh.com>
2019-02-17 05:13:30 -0500

    Locale

--- income_tax_v1.php
+++ income_tax_v1.php
@@ -55,7 +55,8 @@
            */
 
             // Setting local to en_US to use money_format
-            setlocale(LC_MONETARY, 'en_US');
+            $locale = Locale::acceptFromHttp($_SERVER['HTTP_ACCEPT_LANGUAGE']);
+            setlocale(LC_MONETARY, $locale);
 
             // Check if Submit Button is clicked or not
             if (isset($_POST['submit'])) {
